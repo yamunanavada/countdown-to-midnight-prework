4808da95e7d8ae70f4ce02d0df1c2c1b69118f2d
Yamuna Navada <yamunanavada@gmail.com>
2017-10-24 14:30:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,15 @@ def countdown(seconds)
   "HAPPY NEW YEAR!"
 
 end
+
+
+def countdown_with_sleep(seconds)
+
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+
+  "HAPPY NEW YEAR!"
+end
